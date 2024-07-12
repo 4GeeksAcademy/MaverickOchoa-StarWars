8283b6e10e3d25bca14f91431c80e3b924175513
MaverickOchoa <126739291+MaverickOchoa@users.noreply.github.com>
2024-07-12 01:44:49 +0000

    stat startwars

--- src/js/views/details.js
+++ src/js/views/details.js
@@ -5,13 +5,13 @@ import { Context } from "../store/appContext";
 
 import "../../styles/demo.css";
 
-export const Demo = () => {
+export const Details = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="container">
 			<ul className="list-group">
-				{store.demo.map((item, index) => {
+				{store.ditail.map((item, index) => {
 					return (
 						<li
 							key={index}
@@ -21,12 +21,12 @@ export const Demo = () => {
 								<span>Link to: {item.title}</span>
 							</Link>
 							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
+								// Check to see if the background is orange, if so, display the message
+								item.background === "orange" ? (
+									<p style={{ color: item.initial }}>
+										Check store/flux.js scroll to the actions to see the code
+									</p>
+								) : null}
 							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
 								Change Color
 							</button>
